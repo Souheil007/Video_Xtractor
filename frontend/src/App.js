6725ef9f16a121e285885baf14d7eb52f65e0226
Souheil007 <107995350+Souheil007@users.noreply.github.com>
2024-10-26 12:39:12 +0100

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import VideoProcessingApp from './components/VideoProcessingApp';
+import VideoProcessingAppV2 from './components/videoProcessingAppV2';
+
+function App() {
+  return (
+    <div className="App">
+      <VideoProcessingApp />
+    </div>
+  );
+}
+
+export default App;
